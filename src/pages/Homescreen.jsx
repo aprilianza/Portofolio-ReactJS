ab2baca23cc78923f75525573beba6ce8491a3a7
aprilianza <138241892+aprilianza@users.noreply.github.com>
2025-08-10 21:20:10 +0700

    .

--- src/pages/Homescreen.jsx
+++ src/pages/Homescreen.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <div className="homepage">
       <Container className="hero-section">
         <Row className="hero-content">
-          <Col>
+          <Col lg={6}>
             <h1 className="name mb-3">Hallo, I'm Aprilianza,</h1>
             <h1 className="job mb-3">
               Informatics<br></br>Student
@@ -18,7 +18,7 @@ const Home = () => {
               </a>
             </div>
           </Col>
-          <Col className="col-vertical-center">
+          <Col className="col-vertical-center" lg={6}>
             <img src={myimage} alt="My Photo" className="myimage" />
           </Col>
         </Row>
